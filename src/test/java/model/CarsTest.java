8921c0a8cd0e8c0909d9462112cb16d7e8ff3274
kwkim <kkw5657@gmail.com>
2021-05-02 00:43:21 +0900

    test : 자동차 리스트 생성 테스트

---
+++ src/test/java/model/CarsTest.java
@@ -0,0 +1,16 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CarsTest {
+
+    @Test
+    @DisplayName("Cars 생성 테스트")
+    public void Car_생성_Test () {
+        Cars cars = new Cars("ab,bc,cd,de");
+        assertThat(cars.getCars().size()).isEqualTo(4);
+    }
+}
